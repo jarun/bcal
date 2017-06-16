dc0d18060c1819b90d0b699d86d69a08c4492b05
Anupam Roy <sampu@gmx.us>
2017-06-16 21:28:07 +0530

    Bugfix (#11)
    * Corrected Structure return
    
    * corrected problems
    
    * updated
    
    * update
    
    * bugfix

--- bcal.c
+++ bcal.c
@@ -926,7 +926,8 @@ maxuint_t eval(queue **front, queue **rear, int *out)
 				break;
 			case '*':
 				/* Check if only one is unit */
-				if (!raw_a.unit || !raw_b.unit) {
+				if ((!raw_a.unit || !raw_b.unit) &&
+				     (raw_a.unit != raw_b.unit)) {
 					c = a * b;
 					raw_c.unit = 1;
 				} else {
